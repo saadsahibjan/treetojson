a122e972fb2a83998f1f06e737ffb3c58407f429
Saad Sahibjan <saad.sahibjan@gmail.com>
2017-01-26 00:07:31 +0530

    Modify setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,23 @@
 from distutils.core import setup
 
-version = '0.0.1'
+version = '0.0.4'
 
 setup(
     name='treetojson',
     version=version,
     description='Converts a tree structure in to a valid JSON',
-    long_description=open('README.markdown').read(),
+    #long_description=open('README.md').read(),
     author='Saad Sahibjan',
     author_email='saad.sahibjan@gmail.com',
-    license='LICENSE',
+    license='MIT',
     url='https://github.com/saadsahibjan/treetojson',
     py_modules=['treetojson'],
     platforms='Cross-platform',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+    install_requires=['nltk'],
 )
