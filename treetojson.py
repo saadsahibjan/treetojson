3b9c33b8b1dd93a29dc586b6cd2981d032edb5b7
Saad Sahibjan <saad.sahibjan@gmail.com>
2017-01-25 17:07:43 +0530

    Update comment

--- treetojson.py
+++ treetojson.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 """
-Converts a list or a list with a given regex grammar which contains a tree structure into a valid JSON.
+Converts a tree structure in to a valid JSON.
 
 This module works with both Python 2 and 3.
 """
