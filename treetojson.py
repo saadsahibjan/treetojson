aec33e5eaf40deed73c580a170714810229678fc
Saad Sahibjan <saad.sahibjan@gmail.com>
2017-01-25 13:17:34 +0530

    Add get_json method to provide json output

--- treetojson.py
+++ treetojson.py
@@ -31,10 +31,29 @@ def set_debug(debug=True, filename='dicttoxml.log'):
 
 
 def depict_tree(data, grammar=None):
+    """Converts the provided list into a tree structure
+    Arguments:
+    - data contains a list which should look like,
+        [('I', 'NN'), ('am', 'NN'), ('a', 'NN'), ('good', 'VB'), ('boy', 'NN')]
+    - grammar is optional, it accepts NLTK regexp grammar.
+    """
+    LOG.info('Inside depict_tree()')
+
     if grammar:
         parser = RegexpParser(grammar)
     else:
         parser = RegexpParser('''
                 ''')
-    tree = parser.parse(data)
+    return parser.parse(data)
 
+
+def get_json(data, grammar=None):
+    """Provides a JSON output for a given list
+    Arguments:
+    - data contains a list which should look like,
+        [('I', 'NN'), ('am', 'NN'), ('a', 'NN'), ('good', 'VB'), ('boy', 'NN')]
+    - grammar is optional, it accepts NLTK regexp grammar.
+    """
+    LOG.info('Inside get_json()')
+
+    tree = depict_tree(data, grammar=grammar)
