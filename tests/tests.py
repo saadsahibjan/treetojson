627e3256cd5d4d42731adbc3836549b1faf6e872
Saad Sahibjan <saad.sahibjan@gmail.com>
2017-01-26 13:59:38 +0530

    Add test case for list containing words with tags along with grammar

--- tests/tests.py
+++ tests/tests.py
@@ -14,6 +14,24 @@ class TreeToJsonTests(unittest.TestCase):
         self.assertEqual(output, result)
 
 
+def test_listWithGrammar(self):
+    result = "{\"SENTENCE\":[{\"NP\":[{\"NN\":\"Everyone\"}]},{\"VERB\":[{\"VBZ\":\"knows\"}]},{\"COMP\": " \
+             "[{\"DT\":\"an\"},{\"NP\":[{\"NN\":\"Elephant\"}]},{\"VERB\":[{\"VBZ\":\"is\"}]},{\"CP\":" \
+             "[{\"JJR\":\"larger\"}]},{\"THAN\":[{\"IN\":\"than\"}]},{\"DT\":\"a\"},{\"NP\":" \
+             "[{\"NN\":\"Dog\"}]}]}]}"
+    sentence = [('Everyone', 'NN'), ('knows', 'VBZ'), ('an', 'DT'), ('Elephant', 'NN'), ('is', 'VBZ'),
+                ('larger', 'JJR'), ('than', 'IN'), ('a', 'DT'), ('Dog', 'NN')]
+    grammar = """
+        NP:   {<PRP>?<JJ.*>*<NN.*>+}
+        CP:   {<JJR|JJS>}
+        VERB: {<VB.*>}
+        THAN: {<IN>}
+        COMP: {<DT>?<NP><RB>?<VERB><DT>?<CP><THAN><DT>?<NP>}
+        """
+    output = treetojson.get_json(data=sentence, grammar=grammar)
+    self.assertEqual(output, result)
+
+
 def main():
     unittest.main()
 
